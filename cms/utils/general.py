9d3b4fc917439be7987bbf9c14fe97daf5d9bf0c
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-03-29 11:34:02 +0530

    deleted

--- cms/utils/general.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-
-
-def generate_random_string(string_length):
-    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(string_length))
-
-if __name__ == "__main__":
-    print generate_random_string(10)
