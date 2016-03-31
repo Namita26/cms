5bd66e32680d5cc5d430381b3abf42ffdc822a76
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-03-31 17:12:38 +0530

    location changes

--- static/js/main.js
+++ static/js/main.js
@@ -2,17 +2,21 @@ var SERVER_URL = "http://localhost:5000";
 var BASE_URL = "http://localhost";
 
 function validate_token(id_token){
+	console.log("In validate token");
 	$.ajax({
 	      url: SERVER_URL + "/auth",
 	      type: 'GET',
 	      data: {"id_token": id_token},
 	      success: function(data){
 	      	  // TODO : store the token in the cookie using jquery cookie
-	          window.location.href = BASE_URL + "/templates/concept_creation.html"
+	      	  console.log("In succes of idtoken-------------------")
+	      	  console.log(data)
+	          window.location.href = BASE_URL + "/templates/dashboard.html";
 	      },
 	      error: function(data){
 	      	  // TODO : Get the message from the server.
-	      	  window.location.href = BASE_URL + "?error=Error in Authentication"
+	      	  console.log(data);
+	      	  window.location.href = BASE_URL + "/templates/unauthorised.html";
 	      }
 	  });
 }
