272cc67acbe5f2bed1b94f1744398faea7eaa8c5
Namita Maharanwar <namitamaharanwar@gmail.com>
2016-04-04 18:15:09 +0530

    per user

--- service_apis_handlers/get_concept_handler.py
+++ service_apis_handlers/get_concept_handler.py
@@ -5,9 +5,12 @@ import json
 from utils.db.concept import get_all_concepts, get_concept
 
 
-def handle_request(concept_id):
-    if concept_id == 'all':
-        concept = get_all_concepts()
+def handle_request(concept_details):
+    if type(concept_details) == dict:
+        useremail = concept_details['useremail']
+        concept_id = concept_details['concept_id']
+        if concept_id == 'all':
+            concept = get_all_concepts(useremail)
     else:
-        concept = get_concept(long(concept_id))
+        concept = get_concept(long(concept_details))
     return json.dumps(concept)
